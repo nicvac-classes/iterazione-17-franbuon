069a84c9562689bef118a115ffaaff7407fb345d
franbuon <francescogiuseppe.buono@marconibari.edu.it>
2024-10-27 16:53:51 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,47 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    float N, max, min, mediapos, Npos, Nneg, somma;
+    int vneg, vpos;
+    max=0;
+    min=0;
+    vpos=0;
+    vneg=0;
+    somma=0;
+    cout<<"Inserisci il numero"<<endl;
+    cin>>N;
+    while(N!=0)
+    {
+       if(N>0)
+       {
+        Npos=N;
+        vpos=vpos+1;
+        somma=somma+Npos;
+       }
+       else
+       {
+        vneg=vneg+1;
+        Nneg=N;
+       }
+       if(N>max)
+       {
+        max=N;
+       }
+       else
+       {
+        if(N<min)
+        {
+            min=N;
+        }
+       }
+       cout<<"Inserisci il numero"<<endl;
+       cin>>N;
+    }
+    mediapos=somma/vpos;
+    cout<<"La media dei valori positivi è "<<mediapos<<endl;
+    cout<<"I valori negativi sono "<<vneg<<endl;
+    cout<<"Il valore massimo è "<<max<<endl;
+    cout<<"Il valore minimo è "<<min;
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
